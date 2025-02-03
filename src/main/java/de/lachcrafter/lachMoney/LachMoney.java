8380e5b58306e8d52a1b2586e63796206b8a9cef
LachCrafter <lachcrafteryt@gmail.com>
2025-02-03 19:19:31 +0100

    chore: add command registerer

--- src/main/java/de/lachcrafter/lachMoney/LachMoney.java
+++ src/main/java/de/lachcrafter/lachMoney/LachMoney.java
@@ -1,17 +1,30 @@
 package de.lachcrafter.lachMoney;
 
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class LachMoney extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Initializing LachMoney...");
+        regCommands();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void regCommands() {
+        LifecycleEventManager<@NotNull Plugin> manager = this.getLifecycleManager();
+        manager.registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+            final Commands commands = event.registrar();
+        });
     }
 }
