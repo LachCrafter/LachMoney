4e02a508ece13c6d6cf8b9052f800c210a05a431
LachCrafter <lachcrafteryt@gmail.com>
2025-02-03 19:05:45 +0100

    init

---
+++ src/main/java/de/lachcrafter/lachMoney/LachMoney.java
@@ -0,0 +1,17 @@
+package de.lachcrafter.lachMoney;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LachMoney extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
